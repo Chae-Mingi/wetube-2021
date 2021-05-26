481e231896315afab4ecf44f652de514e14c180c
Chae-Mingi <82092393+Chae-Mingi@users.noreply.github.com>
2021-05-26 12:41:55 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -4,14 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+};
 const handleHome = (req, res) => {
-    return res.send("I still love you");
+    return res.send("I love middlewares");
 };
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-};
+app.use(gossipMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT}🚀`);
 
