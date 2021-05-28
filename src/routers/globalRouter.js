b9cf90eed2489d01f66aa1c5045c8cf8abfcd29f
Chae-Mingi <82092393+Chae-Mingi@users.noreply.github.com>
2021-05-28 21:54:00 +0900

    cleaning the code - 2
    cleaning the code by using non default export

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { join } from "../controllers/ueserController"
+import { trending } from "../controllers/videoController"
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", trending);
+globalRouter.get("/", join);
 
-globalRouter.get("/", handleHome);
 
-export default globalRouter;
+export default globalRouter
