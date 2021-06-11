fc8af9ea35f45eb4464a4855386ed0744f4d95d0
Chae-Mingi <82092393+Chae-Mingi@users.noreply.github.com>
2021-06-11 17:46:28 +0900

    Login part one

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,7 @@ export const postJoin = async(req, res) => {
     const {name, username, email, password, password2, location} = req.body;
     const pageTitle = "join";
     if(password !== password2){
-        return res.status(400).render("join",{
+        return res.render("join",{
             pageTitle,
             errorMessage: "Password confirmation does not match.",
         });
@@ -17,17 +17,35 @@ export const postJoin = async(req, res) => {
             errorMessage: "This username/email is already taken.",
         });
     }
-    await User.create({
-        name, 
-        username, 
-        email, 
-        password, 
-        location,
-    });
-    return res.redirect("/login");
+    try {
+        await User.create({
+            name, 
+            username, 
+            email, 
+            password, 
+            location,
+        });
+        return res.redirect("/login");
+    } catch(error) {
+        return res.status(400).render("join", {
+            pageTitle: "Upload Video",
+            errorMessage: error._message,
+        })
+    }
+};
+export const getLogin = (req, res) => 
+    res.render("login", { pageTitle:"Login" });
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const exists = await User.exists({ username });
+    if(!exists){
+        return res.status(400).render("login", { pagTitle: "Login", errorMessage:"An account with this username does not exists."})
+    }
+    // check if account exists
+    // check if password correct
+    res.end();
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
