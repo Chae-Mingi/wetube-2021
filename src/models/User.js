8f8bfa5f510c3bc52d2063385fe2b32bdb4fd172
Chae-Mingi <82092393+Chae-Mingi@users.noreply.github.com>
2021-06-19 23:02:28 +0900

    Github Login part six

--- src/models/User.js
+++ src/models/User.js
@@ -2,16 +2,17 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    password : { type: String, required: true },
-    name: { type: String, required: true },
-    location: String,
+  email: { type: String, required: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
+  username: { type: String, required: true, unique: true },
+  password: { type: String },
+  name: { type: String, required: true },
+  location: String,
 });
 
 userSchema.pre("save", async function () {
-    this.password = await bcrypt.hash(this.password, 5);
-})
+  this.password = await bcrypt.hash(this.password, 5);
+});
 
 const User = mongoose.model("user", userSchema);
 export default User;
