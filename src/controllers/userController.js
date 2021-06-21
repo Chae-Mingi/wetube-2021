2bd7a748a66b18057543f8f076f5737843ae8228
Chae-Mingi <82092393+Chae-Mingi@users.noreply.github.com>
2021-06-21 12:42:35 +0900

    File Uploads part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -143,12 +143,11 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
   } = req;
-  console.log(file);
   const exists = await User.exists({
     $and: [{ _id: { $ne: _id } }, { $or: [{ username }, { email }] }],
   });
@@ -161,6 +160,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
